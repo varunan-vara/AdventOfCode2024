6de112b03e7c9bd677972dc6294365cbda29dce3
Varunan Varathan <varunanvarathan@Varunans-MacBook-Air.local>
2024-12-16 21:16:03 -0500

    Refactored all my code, since I switched computers

---
+++ AdventOfCode2024/Solutions.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef ADVENT_OF_CODE_SOLUTIONS
+#define ADVENT_OF_CODE_SOLUTIONS
+
+void dayOne();
+void dayTwo();
+void dayThree();
+void dayFour();
+void dayFive();
+void daySix();
+
+#endif ADVENT_OF_CODE_SOLUTIONS
--- README.md
+++
@@ -1,5 +0,0 @@
-# AdventOfCode2024
-
-Code is in 'AdventOfCode2024Sol' Folder
-
-Project is for Visual Studio 2022
